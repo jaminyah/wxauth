8958d109f0ceacc6befbc2098185c68e1c942142
jaminyah <jaminyah@gmail.com>
2021-04-25 19:21:45 -0400

    Add database test stubs

---
+++ platform/database/database_test.go
@@ -0,0 +1,35 @@
+package database
+
+import "testing"
+
+func TestInit(t *testing.T) {
+
+}
+
+func TestInsertUser(t *testing.T) {
+
+}
+
+func TestGetUser(t *testing.T) {
+
+}
+
+func TestReadUsers(t *testing.T) {
+
+}
+
+func TestDeleteUser(t *testing.T) {
+
+}
+
+func TestUpdateMail(t *testing.T) {
+
+}
+
+func TestUpdateToken(t *testing.T) {
+
+}
+
+func TestUpdateServices(t *testing.T) {
+
+}
